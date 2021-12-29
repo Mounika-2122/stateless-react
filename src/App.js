2dcb2fd952240b3120904f9f46c2f165dfd66951
Mounika <bojjamounika8@gmail.com>
2021-12-29 13:02:16 +0530

    stateless

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const college = (props) => {
+   return (
+      <div>
+         <p> Hello Everyone {props.name} Good Afternoon cgcs.
+         {props.course}</p>
+         {props.student}
+      </div>
+   );
+}
+export default college;
